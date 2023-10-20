b1d682f2182822dd7978602dfc32470a97341fca
Nick Young <nick.young@auckland.ac.nz>
2023-10-20 13:37:35 +1300

    improve code performance 3x

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ from tqdm.auto import tqdm
 
 BASE_YEAR = 1940
 FUTURE_YEAR = 2100
-SUPPORTED_MODELS = ["linear", "double", "sqrt", "BH", "Sunamura"]
+SUPPORTED_MODELS = ["linear", "sqrt", "BH", "Sunamura"]
 
 
 def enrich_df(df: pd.DataFrame):
@@ -33,16 +33,18 @@ def calculate_new_coordinates(old_x, old_y, bearing, distance):
 def predict(
     df: pd.DataFrame,
     azimuth_lookup: dict,
-    model="linear",
     Historic_SLR=0.002,
     Proj_SLR=0.01,
+    Length_AP=82.1,
+    Prop=0.1,
+    B_Height=20,
+    C_Depth=9.577
 ):
     """_summary_
 
     Args:
         df (pd.DataFrame): dataframe with columns: TransectID, Date, Distance, YearsSinceBase
         azimuth_lookup (dict): dict lookup of TransectID to Azimuth
-        model (str, optional): a model from SUPPORTED_MODELS. Defaults to "linear".
         Historic_SLR (float, optional): Historic Sea Level Rise, only used for the SQRT, BH and Sunamura models. Defaults to 0.002.
         Proj_SLR (float, optional): Projected Sea Level Rise, only used for the SQRT, BH and Sunamura models. Defaults to 0.01.
 
@@ -52,10 +54,6 @@ def predict(
     Returns:
         pd.DataFrame: resulting prediction points for the year 2100
     """
-    if model not in SUPPORTED_MODELS:
-        raise ValueError(
-            f"Model {model} not supported. Supported models: {SUPPORTED_MODELS}"
-        )
     grouped = df.groupby("TransectID")
     results = []
     for group_name, group_data in tqdm(grouped):
@@ -63,60 +61,52 @@ def predict(
             continue
         coefficients = np.polyfit(group_data.YearsSinceBase, group_data.Distance, 1)
         slope = coefficients[0]
-        if model == "double":
-            slope *= 2
-        elif model == "sqrt":
-            # Walkden and Dickson sqrt relationship
-            SQRT = (Proj_SLR / Historic_SLR) ** 0.5
-            slope *= SQRT
-        elif model == "BH":
-            # Bray and Hooke relationship inputs
-            Length_AP = 82.1
-            Prop = 0.1
-            B_Height = 20
-            C_Depth = 9.577
-
-            # Bray and Hooke relationship
-            X = Prop * (B_Height + C_Depth)
-            Y = Length_AP / X
-            Z = (Proj_SLR - Historic_SLR) * Y
-            slope += Z
-        elif model == "Sunamura":
-            # Sunamara relationship inputs
-            Length_AP = 82.1
-            C_Depth = 9.577
-
-            # Bray and Hooke relationship
-            X = C_Depth / (slope + Length_AP)
-            Y = (Proj_SLR - Historic_SLR) / X
-            slope += Y
-
-        intercept = coefficients[1]
         # Erosion only
-        if slope < 0:
+        if slope > 0:
+            continue
+
+        latest_row = group_data[group_data.Date == group_data["Date"].max()].iloc[0]
+        result = {
+            "TransectID": group_name,
+            "Year": FUTURE_YEAR,
+            "ocean_point": calculate_new_coordinates(
+                latest_row.geometry.x,
+                latest_row.geometry.y,
+                azimuth_lookup[group_name] + 180,
+                500,
+            ),
+        }
+
+        for model in SUPPORTED_MODELS:
+            slope = coefficients[0]
+            intercept = coefficients[1]
+            if model == "linear":
+                pass
+            elif model == "sqrt":
+                # Walkden and Dickson sqrt relationship
+                SQRT = math.sqrt(Proj_SLR / Historic_SLR)
+                slope *= SQRT
+            elif model == "BH":
+                # Bray and Hooke relationship
+                X = Prop * (B_Height + C_Depth)
+                Y = Length_AP / X
+                Z = (Proj_SLR - Historic_SLR) * Y
+                slope += Z
+            elif model == "Sunamara":
+                X = C_Depth / (slope + Length_AP)
+                Y = (Proj_SLR - Historic_SLR) / X
+                slope += Y
+
             predicted_distance = slope * (FUTURE_YEAR - BASE_YEAR) + intercept
-            latest_row = group_data[group_data.Date == group_data["Date"].max()].iloc[0]
             distance_difference = latest_row.Distance - predicted_distance
-            results.append(
-                {
-                    "TransectID": group_name,
-                    "Year": FUTURE_YEAR,
-                    "Distance": predicted_distance,
-                    "geometry": calculate_new_coordinates(
-                        latest_row.geometry.x,
-                        latest_row.geometry.y,
-                        azimuth_lookup[group_name],
-                        distance_difference,
-                    ),
-                    "ocean_point": calculate_new_coordinates(
-                        latest_row.geometry.x,
-                        latest_row.geometry.y,
-                        azimuth_lookup[group_name] + 180,
-                        500,
-                    ),
-                }
+            result[f"{model}_model_point"] = calculate_new_coordinates(
+                latest_row.geometry.x,
+                latest_row.geometry.y,
+                azimuth_lookup[group_name],
+                distance_difference,
             )
-    results = gpd.GeoDataFrame(results, crs=2193)
+        results.append(result)
+    results = gpd.GeoDataFrame(results)
     return results
 
 
@@ -139,8 +129,9 @@ if __name__ == "__main__":
         df.crs = 2193
         df = enrich_df(df)
         azimuth_lookup = get_azimuth_dict(f"Shapefiles/{site}_TransectLines.shp")
-        for model in tqdm(SUPPORTED_MODELS):
-            results = predict(df, azimuth_lookup, model)
+        results = predict(df, azimuth_lookup)
+        for model in SUPPORTED_MODELS:
+            results.set_geometry(f"{model}_model_point", inplace=True, crs=2193)
             polygon = prediction_results_to_polygon(results)
             output_shapefile = f"Projected_Shoreline_Polygons/{site}_{model}.shp"
             polygon.to_file(output_shapefile, driver="ESRI Shapefile")
